f967604b388275d06b1441663ffbc3e8ca5af6a2
Karen Garcia <karen.garcia@nexscreen.com>
2019-10-06 20:20:52 -0600

    initial repository

---
+++ precache-manifest.e2461fe0199ec3b749ebbc7f200f4642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f2ad49637a10cec0c7953dc9f11258",
+    "url": "/ColorsRedux/index.html"
+  },
+  {
+    "revision": "0d1fd6d4054ea8edc487",
+    "url": "/ColorsRedux/static/css/main.956bc47f.chunk.css"
+  },
+  {
+    "revision": "d8ca8e428a695cf64d19",
+    "url": "/ColorsRedux/static/js/2.b3e46991.chunk.js"
+  },
+  {
+    "revision": "0d1fd6d4054ea8edc487",
+    "url": "/ColorsRedux/static/js/main.679c6790.chunk.js"
+  },
+  {
+    "revision": "13b30f9379bc795bc704",
+    "url": "/ColorsRedux/static/js/runtime~main.b741e1ea.js"
+  }
+]);
